3504516fc1cc3a4bf37548a7a9a300cb6cd0611b
Bohdan <zinyshen@gmail.com>
2024-11-06 15:13:57 +0200

    update

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types, Router
-from aiogram.types import Update
-from fastapi import FastAPI, Request
-import config  # Предполагается, что config.py содержит Token и WEBHOOK_URL
+import telebot
+from flask import Flask, request
+import config  # файл конфигурации с настройками
+from telebot import types
+import psycopg2
 
-# Инициализация бота и диспетчера
-bot = Bot(token=config.Token, parse_mode="HTML")
-dp = Dispatcher()  # Dispatcher теперь инициализируется без параметров
-router = Router()  # Создаем Router для регистрации хендлеров
-dp.include_router(router)
+# Инициализация бота
+bot = telebot.TeleBot(config.Token, threaded=False)
+app = Flask(__name__)
 
-# Инициализация FastAPI
-app = FastAPI()
+
+# Установка вебхука
+bot.remove_webhook()
+bot.set_webhook(url=config.WEBHOOK_URL + config.SECRET_PATH)
+
+# Подключение к базе данных PostgreSQL
+try:
+    conn = psycopg2.connect(config.url_database_telegram)
+    cursor = conn.cursor()
+    print("Подключение к PostgreSQL успешно!")
+except Exception as e:
+    print("Ошибка при подключении к базе данных:", e)
+
 
 # Обработчик команды /start
-@router.message(commands=["start"])
-async def cmd_start(message: types.Message):
-    await message.answer("Привет! Я простой телеграм-бот.")
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я твой бот и готов к работе!")
 
-# Роут для вебхука
-@app.post("/webhook")
-async def telegram_webhook(request: Request):
-    update = await request.json()
-    telegram_update = Update(**update)
-    await dp.process_update(telegram_update)
-    return {"ok": True}
 
-# Установка вебхука при запуске FastAPI
-@app.on_event("startup")
-async def on_startup():
-    # Устанавливаем вебхук для бота
-    await bot.set_webhook(config.WEBHOOK_URL)
+# Обработка вебхука от Telegram
+@app.route('/webhook_update' + config.WEBHOOK_URL, methods=['POST'])
+def webhook():
+    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+    bot.process_new_updates([update])
+    return 'ok', 200
 
-# Удаление вебхука при остановке FastAPI
-@app.on_event("shutdown")
-async def on_shutdown():
-    # Удаляем вебхук, когда приложение выключается
-    await bot.delete_webhook()
-
+# # Запуск приложения
+# if __name__ == "__main__":
+#     app.run()
--- config.py
+++ config.py
@@ -8,3 +8,5 @@ Token = os.getenv("API_TOKEN_TELEGRAM")
 url_database_telegram = os.getenv("URL_DATABASE")
 
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+SECRET_PATH = os.getenv("SECRET_PATH")
